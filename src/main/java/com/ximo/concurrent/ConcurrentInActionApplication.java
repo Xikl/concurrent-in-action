3df0beb4982fde1b6540cd5af8738369af5b07ff
朱文赵 <zwz@servyou.com.cn>
2018-01-17 15:37:51 +0800

    go

---
+++ src/main/java/com/ximo/concurrent/ConcurrentInActionApplication.java
@@ -0,0 +1,12 @@
+package com.ximo.concurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentInActionApplication.class, args);
+	}
+}
