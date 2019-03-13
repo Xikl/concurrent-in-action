a166e854ae3c6dce4a6ac8078014f91566f6bde1
xikl <1392936858@qq.com>
2019-03-13 22:17:05 +0800

    对锁的理解

--- effective-java-concurrent/src/main/java/com/ximo/efc/effectivejava/chap10/tip67/ObservableSet.java
+++ effective-java-concurrent/src/main/java/com/ximo/efc/effectivejava/chap10/tip67/ObservableSet.java
@@ -4,6 +4,7 @@ package com.ximo.efc.effectivejava.chap10.tip67;
 import com.ximo.efc.effectivejava.chap04.tip18.ForwardingSet;
 
 import java.util.*;
+import java.util.concurrent.*;
 import java.util.stream.IntStream;
 
 /**
@@ -68,17 +69,12 @@ public class ObservableSet<E> extends ForwardingSet<E> {
     }
 
     public static void main(String[] args) {
-        ObservableSet<Integer> set = new ObservableSet<>(new HashSet<>());
-        set.addObserver(new SetObserver<Integer>() {
-            @Override
-            public void added(ObservableSet<Integer> set, Integer element) {
-                System.out.println(element);
-                if (element == 23) {
-                    set.removeObserver(this);
-                }
-            }
-        });
+//        testObserverWhenError();
+        testObserverBySubThread();
+    }
 
+    public static void testObsever() {
+        ObservableSet<Integer> set = new ObservableSet<>(new HashSet<>());
 
         // 创建observer实例
         SetObserver<Integer> setObserver = (s, e) -> System.out.println(e);
@@ -89,5 +85,55 @@ public class ObservableSet<E> extends ForwardingSet<E> {
         IntStream.rangeClosed(0, 100).forEach(set::add);
     }
 
+
+    /**
+     * 他会打印1 - 23的数字 然后抛出异常
+     * 因为一个正在遍历的集合 然后 它删除了
+     * Exception in thread "main" java.util.ConcurrentModificationException
+     */
+    private static void testObserverWhenError() {
+        ObservableSet<Integer> set = new ObservableSet<>(new HashSet<>());
+
+        set.addObserver(new SetObserver<Integer>() {
+            @Override
+            public void added(ObservableSet<Integer> set, Integer element) {
+                System.out.println(element);
+                if (element == 23) {
+                    set.removeObserver(this);
+                }
+            }
+        });
+
+        // 遍历添加
+        IntStream.rangeClosed(0, 100).forEach(set::add);
+    }
+
+
+    private static void testObserverBySubThread() {
+        ObservableSet<Integer> set = new ObservableSet<>(new HashSet<>());
+
+        // 由于主线程占有 observers这个锁 然后子线程 会一直得到不该锁 主线程也一直再等他 所以会导致死锁
+        // 解决办法就是加上超时时间
+        set.addObserver(new SetObserver<Integer>() {
+            @Override
+            public void added(ObservableSet<Integer> set, Integer element) {
+                System.out.println(element);
+                if (element == 23) {
+                    ExecutorService executorService = Executors.newSingleThreadExecutor();
+                    try {
+                        executorService.submit(() -> set.removeObserver(this)).get(3, TimeUnit.SECONDS);
+                    } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                        e.printStackTrace();
+                    }finally {
+                        // 关闭线程池
+                        executorService.shutdown();
+                    }
+                }
+            }
+        });
+
+        // 遍历添加
+        IntStream.rangeClosed(0, 100).forEach(set::add);
+    }
 
 }
