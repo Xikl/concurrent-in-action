4f470e6ba76fecf30b13c7103b0564df11ec156b
朱文赵 <1392936858@qq.com>
2019-05-14 01:28:30 +0800

    静态条件

---
+++ thinking-and-programming/src/main/java/com/ximo/thinkingandprogramming/stage3/the7conucy/Counting.java
@@ -0,0 +1,53 @@
+package com.ximo.thinkingandprogramming.stage3.the7conucy;
+
+/**
+ * @author xikl
+ * @date 2019/5/14
+ */
+public class Counting {
+
+    static class Counter {
+        private int count = 0;
+
+        public void increment() {
+            ++count;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+    }
+
+
+    static class CountingThread extends Thread {
+
+        private Counter counter;
+
+        public CountingThread(Counter counter) {
+            this.counter = counter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+        CountingThread countingThread1 = new CountingThread(counter);
+        CountingThread countingThread2 = new CountingThread(counter);
+
+        countingThread1.start();
+        countingThread2.start();
+        countingThread1.join();
+        countingThread2.join();
+
+        System.out.println(counter.getCount());
+    }
+}
+
+
